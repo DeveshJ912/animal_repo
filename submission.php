8d6ab75674aa92ab382704c2ad164f02eacf3b04
Devesh Joharapurkar <devesh9j@gmail.com>
2021-09-28 17:19:53 +0530

    updated submission file

--- submission.php
+++ submission.php
@@ -2,109 +2,231 @@
 <html lang="en">
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+        <link rel="stylesheet" type="text/css" href="style.css">
         <title>Submission</title>
-        <style>
-            .con{
-                align-self: center;
-                height: 500px;
-                width: 450px;
-                padding: 20px;
-                margin-left: 430px;
-                border: 2px black solid;
-            }
-
-            #formid{
-                padding-top: 30px;
-                padding-left: 35px;
-            }
-
-            #desc {
-                    margin-left: 145px;
-                    margin-top: -12px;
-                }
-
-            #ver{
-                align-self: center;
-                background-color: #3251b9; /* Green */
-                border: none;
-                color: white;
-                padding: 10px 20px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                font-size: 16px;
-                margin-right: 30px;
-                cursor: pointer;
-                margin-left: 70px;
-            }
-            #submitbutton{
-                background-color: #678a68; /* Green */
-                border: none;
-                color: white;
-                padding: 10px 20px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                font-size: 16px;
-            }
-            .captcha{
-                margin-left: 80px;
-            }
-        </style>
     </head>
+
+
+    <?php
+
+    #variable initialization
+    $nameErr = $imgErr = $catErr = $descErr = $captchaErr= "";
+    $name = $img = $category = $lifeErr = $desc = "";
+    $valid ="";
+    $num1=$num2=0;
+    
+    
+# if form submitted by post method
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    #adding of captcha numbers
+    $sum=$_POST['num1']+$_POST['num2'];
+    $valid = true;
+
+    #checking if name is empty or not
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+    $valid=false;
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+
+  #checking if img is empty or not
+  if (empty($_FILES['img']['tmp_name'])) {
+    $imgErr = "Image is required";
+    $valid=false;
+  } else {
+    $img = ($_FILES["img"]);
+    
+  }
+    
+  #checking if description is empty or not
+  if (empty($_POST["desc"])) {
+    $descErr = "Description is required";
+    $valid=false;
+  } else {
+    $desc = test_input($_POST["desc"]);
+  }
+
+  #checking if life expectation is empty or not
+  if (($_POST['life'])=='select') {
+    $lifeErr = "Life Expectancy is required";
+    $valid=false;
+  } else {
+    $life = test_input($_POST["life"]);
+    ?>
+    <!-- to select life expectancy from select dropdown -->
+    <script>
+        var selected_life='<?php echo $life;?>';
+        
+        </script>
+    <?php
+  }
+
+  #checking if captcha is correct or not
+  if (($_POST['number'])==$sum) {
+    $valid=true;
+  } else {
+    $captchaErr = "Captcha Error";
+    $valid=false;
+  }
+
+  #checking if category is empty or not
+  if (empty($_POST['category'])) {
+    $catErr = "Category is required";
+    $valid=false;
+  } else {
+    $category = test_input($_POST['category']);
+    
+    ?>
+    <!-- to select category from radio button -->
+    <script>
+        var selected_category='<?php echo $category;?>';
+        
+        </script>
+    <?php
+  }
+}
+
+
+# $valid variable is true if all form values are present.
+if($valid){
+    
+
+    # to connect database
+    $conn=mysqli_connect('localhost','root','','animaldata');
+    
+    #if form submitted get values from post method
+    if(isset($_POST['submit'])){
+        $name=$_POST['name'];
+        $img=$_FILES['img'];
+        $life=$_POST['life'];
+        $desc=$_POST['desc'];
+        $category=$_POST['category'];
+        
+        #to save image in uploads folder
+        $target_Path = "uploads/";
+        $target_Path = $target_Path.basename( $_FILES['img']['name'] );
+        move_uploaded_file( $_FILES['img']['tmp_name'], $target_Path );
+        $path = "uploads/".$_FILES['img']['name'];
+       
+        # insert data in sql table
+        $sql = "INSERT INTO animal(name, category,img,descinfo,life)VALUES('$name','$category','$path','$desc','$life')";
+        if ($conn->query($sql) === TRUE) {
+            #redirect to animal list page
+            header('Location: '.'animal.php');
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+
+    else{
+        echo "error";
+    }
+
+    
+}
+
+#to remove extra blank spaces and slashes
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return ($data);
+}
+?>
+
+
     <body>
         <h1 style="text-align: center;"> Please fill animal details below</h1>
-
+        
         <div class="con" >
-            <form action="add.php" method="POST" enctype="multipart/form-data" id="formid">
-                <label for="name"><b>Animal name:</b></label>
-                <input style="margin-bottom: 20px;" type="text" required="required" id="name" name="name" placeholder="Tommy"><br>
-
-                <label for="Category"><b>Category:</b></label>
-                <input style="margin-left: 4.1%;margin-top: -10%;" type="radio" id="herb" name="category" checked value="herbivores">
-                <label >herbivores</label><br>
-                <input style="margin-left: 21.4%;margin-top: -10%;" type="radio" id="omni" name="category" value="omnivores">
-                <label >omnivores</label><br>
-                <input style="margin-left: 21.4%;margin-top: -10%;" type="radio" id="carn" name="category" value="carnivores">
-                <label >carnivores</label><br>
-                <br>
-                <label for="imgfile"><b>Select an Image:</b></label>
-                <input type="file" id="img" name="img" onchange="return fileValidation()"><br>
-                <sub > Allowed file types: JPG, JPEG, PNG only.</sub>
-                <br>
-                <br>
-                <div id="ad">
-                <label style="margin-top:-10px;" for="desc"><b>Animal Description:</b></label>
+        <p><span class="error">* required field</span></p>  
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" enctype="multipart/form-data" id="formid">
+                <!-- Name element in form -->
+                <div class="name">
+                    <label for="name"><b>Animal name:</b></label>
+                    <input type="text"  id="name" name="name" placeholder="Tommy" value="<?php echo isset($_POST["name"]) ? $_POST["name"] : ''; ?>">
+                    <span class="error">* <?php echo $nameErr;?></span>
                 </div>
-         
-                <textarea id="desc" name="desc" rows="3" cols="30"></textarea><br><br>
+                <!-- category element in form -->
+                <div class="category">
+                    <label for="Category"><b>Category:</b></label>
+                    <span class="error">* <?php echo $catErr;?></span>
+                    <div class="radio">
+                        <input type="radio" id="herbivores" name="category"  value="herbivores">
+                        <label >herbivores</label><br>
+                        <input type="radio" id="omnivores" name="category" value="omnivores">
+                        <label >omnivores</label><br>
+                        <input type="radio" id="carnivores" name="category" value="carnivores">
+                        <label >carnivores</label><br>
+                    </div>
+                </div>
+                <!-- image element in form -->
+                <div class="image">
+                    <label for="imgfile"><b>Select an Image:</b></label>
+                    <span class="im">
+                        <input type="file" id="img" name="img"  onchange="return fileValidation()" value="<?php echo isset($_POST["img"]) ? $_POST["img"] : ''; ?>">
+                    </span>
+                    <span class="error">* <?php echo $imgErr;?></span>
+                    <br>
+                    <sub > Allowed file types: JPG, JPEG, PNG only.</sub>
+                </div>
+                
+                <!-- description element in form -->
+                <div class="description">
+                    
+                    <label for="desc" ><b>Animal Description:</b></label><span class="error">* <?php echo $descErr;?></span>
+                    <br>
+                    <br>
+                    <textarea id="desc" name="desc" rows="3" cols="30"  ><?php echo $desc;?></textarea>
+                    
+                </div>
 
+                <!-- life expectancy element in form -->
+                <div class="life">
                 <label for="life"><b>Life expectancy :</b></label>
-                <select id="life" name="life">
-                    <option value="0-1 year">0-1 year</option>
-                    <option value="1-5 years">1-5 years</option>
-                    <option value="5-10 years">5-10 years</option>
-                    <option value="10+ years">10+ years</option>
-                </select>
-                <br>
-                <br>
-                <br>
-                <div class="captcha">
-                    <label for="cap"><b>&emsp;&emsp;Captcha:&nbsp;</b></label><label id="fn"></label>+<label id="sn"></label>
-                <br>
-                <br>
-                <input type="text" id="cap" name="cap" value=""  placeholder="Enter captcha here"><br><br>
+                    <select id="life" name="life">
+                        <option value="select">select</option>
+                        <option id="0-1 year" value="0-1 year">0-1 year</option>
+                        <option id="1-5 years" value="1-5 years">1-5 years</option>
+                        <option id="5-10 year" value="5-10 years">5-10 years</option>
+                        <option id="10+ years" value="10+ years">10+ years</option>
+                    </select>
+                    <span class="error">* <?php echo $lifeErr;?></span>
                 </div>
-                
-                <button type="button" id="ver" onclick="verify()">verify</button>
-                
-                <input  id = "submitbutton"  disabled type="submit" name="submit" value="Submit">
+                <br>
+
+                <!-- captcha element in form -->
+                <div id="captcha">
+                    <?php $num1=rand(1,100);
+                          $num2=rand(1,100);?>
+                    <input type="hidden" name="num1" value="<?php echo $num1; ?>">
+                    <input type="hidden" name="num2" value="<?php echo $num2; ?>">
+                    <label id ="captchacolor" for="captcha"><b>Captcha :</b> &emsp;</label><?php echo "$num1+$num2";?>
+                    <span class="error">* <?php echo $captchaErr;?></span>
+                    <br>
+                    <br>
+                    <input type="number"  id="number" name="number" placeholder="Enter Captcha Here">
+                    <br>
+                </div>
+                <br>
+                <!-- submit button in form -->
+                <input  id = "submit"   type="submit" name="submit" value="Submit">
                 
             </form>
-
+            
+            
         </div>
         <script>
+            
+            // checked submitted radio category by user
+            document.getElementById(selected_category).checked=true;
+
+            // select submitted life expectancy by user
+            document.getElementById(selected_life).selected=true;
+
 
             function fileValidation() {
                 var fileInput = document.getElementById('img');
@@ -121,26 +243,6 @@
             } 
         }
 
-            var first=document.getElementById("fn").innerHTML = Math.floor(Math.random() * 100);
-            var second=document.getElementById("sn").innerHTML = Math.floor(Math.random() * 100);
-            console.log(first+' '+second);
-
-            var sum=parseInt(first)+parseInt(second);
-            console.log(sum);
-            function verify(){
-                console.log("vgvvjv "+document.getElementById("cap").value);
-
-                var user = document.getElementById("cap").value;
-                if(sum==user){
-                    //document.getElementById("submitbutton").style.display="block";
-                    document.getElementById("submitbutton").disabled=false;
-                    document.getElementById("submitbutton").style.backgroundColor="#4CAF50";
-                    document.getElementById("submitbutton").style.cursor="pointer";
-                }
-                else{
-                    alert("wrong captcha");
-                }
-            }
         </script>
     </body>
 </html>
